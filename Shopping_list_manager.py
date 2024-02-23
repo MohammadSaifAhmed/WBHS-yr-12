6311363662a1c4d8093e046840232e243b18e006
MohammadSaifAhmed <ma21487@my.westlake.school.nz>
2024-02-23 14:06:26 +1300

    shopping updated

--- Shopping_list_manager.py
+++ Shopping_list_manager.py
@@ -12,17 +12,17 @@ shopping_list = {
 def add_to_list(item,price):
     shopping_list.update({item:price})
 
+
 def total_price():
 
     total = 0
     for i in shopping_list.values():
         total += i
      
-
     print(total)
 
-def find_item(item):
 
+def find_item(item):
 
     if item in shopping_list:
         print(shopping_list[item])
@@ -32,7 +32,8 @@ def find_item(item):
         add_the_item = input("Do you want to add the item to your list? y or n: ")
 
         if add_the_item == 'y':
-            add_to_list()
+            price = input("What is the price of the item?: ")
+            add_to_list(item,price)
 
         elif add_to_list == 'n':
             pass
@@ -41,21 +42,20 @@ def find_item(item):
             print("Incorrect input")
 
 
-
-
 while True:
 
     choice = input("What are you planning to do, add to list(1), total price(2), find item(3) , see list(4), Quit(Q):")
 
 
-    if choice == "1":
+    if choice == "1":        
+        item_name = input("What is the name of the item you want to add? :").lower().replace(' ','')
         
-        item_name = input("What is the name of the item you want to add? :")
+        print(item_name)
         price_value = input("What is the price of the item? :")
+
         add_to_list(item_name,price_value)
 
     elif choice == '2':
-
         print("The total price is :" ,total_price())
 
     elif choice == '3':
