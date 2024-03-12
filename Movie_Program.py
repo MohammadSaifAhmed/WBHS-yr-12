2742293bb170ae2fd1a4bb9803025d76212a3fc6
MohammadSaifAhmed <ma21487@my.westlake.school.nz>
2024-03-13 12:49:09 +1300

    movie unfinished

--- Movie_Program.py
+++ Movie_Program.py
@@ -5,44 +5,39 @@ movies = {
                                  "Genre": "Drama",
                                  "Duration": 142,
                                  "Showtime":2200,
-                                 "Today's Ticket sold":143
+                                 "Today's Tickets sold":143
                                 },
     "The Godfather": {
                                  "Genre": "Drama",
                                  "Duration": 175,
                                  "Showtime":1530,
-                                 "Today's Ticket sold":111
+                                 "Today's Tickets sold":111
                                 },
     "Back to the Future": {
                                  "Genre": "Comedy",
                                  "Duration": 116,
                                  "Showtime":1200,
-                                 "Today's Ticket sold":102
+                                 "Today's Tickets sold":102
                                 },
     "Spirited Away": {
                                  "Genre": "Family",
                                  "Duration": 125,
                                  "Showtime":930,
-                                 "Today's Ticket sold":98
+                                 "Today's Tickets sold":98
                                 },
     "The Lion King": {
                                  "Genre": "Family",
                                  "Duration": 88,
                                  "Showtime":700,
-                                 "Today's Ticket sold":123
+                                 "Today's Tickets sold":123
                                 },
 }
 
-def movie_change(type):
+def movie_change():
     input_list = ["Name", "genre", "duration", "showtime","tickets sold"]
     message = ''
-    if type == new:
-        pass
-    elif type == update_movie:
-        movie_search = choicebox("What movie do you want to change?", choices = list(movies.keys()))
-    else:
-        pass
-   
+  
+        
     output = multenterbox("movie details", "Add movie", input_list)
     print(output)
     movies.update({output[0]:{
@@ -50,6 +45,7 @@ def movie_change(type):
             "Duration":output[2],
             "Showtime":output[3],
             "Today's Ticket sold":output[4]
+
 
 
     }})
@@ -75,37 +71,36 @@ def display(movie_name):
             message += (f"""{i} :  \nGenre: {movies[i]['Genre']} \
     \nDuration: {movies[i]['Duration']}min  \
     \nShowtime: {movies[i]['Showtime']} \
-    \nToday's tickets sold: {movies[i]["Today's Ticket sold"]} \n\n""")
+    \nToday's tickets sold: {movies[i]["Today's Tickets sold"]} \n\n""")
 
-    msgbox(message)
+    return message
 
 
 
 
 def update():
    
-    change_options = []
-    
-    for i in movies:
-        if i == movie_search:
-            message += (f"""{i} :  \nGenre: {movies[i]['Genre']} \
-    \nDuration: {movies[i]['Duration']}min  \
-    \nShowtime: {movies[i]['Showtime']} \
-    \nToday's tickets sold: {movies[i]["Today's Ticket sold"]} \n\n""")
+    change_options = ["Name", "Genre", "Duration", "Showtime","Today's Tickets sold"]
+    message = ''
+    movie_name = choicebox("Which movie do you want to update? :" , choices = list(movies.keys()))
+    selection = buttonbox("What do you want to change? :" , choices = change_options)
 
-    for key, value in movies.items():
-        print(f"{key} + {value}") 
-    buttonbox(message)
+    new_value = enterbox(f"Current {selection}: {movies[movie_name][selection]}")
+    movies[movie_name][selection] = new_value
+    msgbox(display(movie_name))
+
+    return 1
+
+
 
 def output_movies():
     message = ''
     for i in movies:
-        message += (f"""{i} :  \nGenre: {movies[i]['Genre']} \n\
-Duration: {movies[i]['Duration']}min  \n\
-Showtime: {movies[i]['Showtime']} \n\
-Today's tickets sold: {movies[i]["Today's Ticket sold"]} \n\n""")
+        message += (display(i))
 
-    msgbox(message)
+    for key, value in movies.items():
+        print(f"{key} + {value}") 
+    buttonbox(message)
 
 
 def leave():
@@ -117,11 +112,12 @@ def main():
 
 
     options = {
-        "Add new movie":new_movie,
+        "Add new movie":movie_change,
         "Find movie showing":search,
         "Update":update,
         "Output Movies":output_movies,
         "Exit":leave,
+     
         
     }
 
@@ -132,16 +128,18 @@ def main():
         title = "movie theater"
 
         choices = (list(options.keys()))
-        print(choices)
+        
 
         selection = buttonbox(msg,title,choices)
-        print(type(selection))
-        print(options[selection])
-        user_choice = options[selection]()
-
-
-        if not user_choice:
+        
+        if not options.get(selection):
             running = False
+        else:
+            user_choice = options[selection]()
+
+
+            if not user_choice:
+                running = False
 
 
 main()
